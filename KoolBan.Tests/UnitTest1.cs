5f70f2797a87590b87242042b50621de629bdd18
warreq <brandon.s.lucas@gmail.com>
2014-11-06 09:35:47 +0100

    init with deps

---
+++ KoolBan.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace KoolBan.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ KoolBan/Scripts/app/kanban.viewmodel.js
@@ -0,0 +1,45 @@
+﻿function KanbanViewModel(app, dataModel) {
+
+    Sammy(function () {
+        this.get('#home', function () {
+            // Make a call to the protected Web API by passing in a Bearer Authorization Header
+            $.ajax({
+                method: 'get',
+                url: app.dataModel.userInfoUrl,
+                contentType: "application/json; charset=utf-8",
+                headers: {
+                    'Authorization': 'Bearer ' + app.dataModel.getAccessToken()
+                },
+                success: function (data) {
+                    self.myHometown('Your Hometown is : ' + data.hometown);
+                }
+            });
+        });
+        this.get('/', function () { this.app.runRoute('get', '#home'); });
+    });
+
+        $('.tile').draggable({
+            revert: 'invalid'
+        });
+        $('td').droppable({
+            accept: '.tile',
+            tolerance: 'pointer',
+            drop: function (event, ui) {
+                alert('inside');
+            },
+            over: function (event, ui) {
+                $('#log').text('over');
+            },
+            out: function (event, ui) {
+                $('#log').text('out');
+            }
+        });
+
+    return self;
+}
+
+app.addViewModel({
+    name: "Kanban",
+    bindingMemberName: "board",
+    factory: KanbanViewModel
+});
