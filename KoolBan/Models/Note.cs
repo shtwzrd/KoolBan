6b6c1de447f95d51288b5808e64150706b7b5ff0
warreq <brandon.s.lucas@gmail.com>
2014-11-08 00:30:41 +0100

    hidden config buttons

--- KoolBan/Models/Note.cs
+++ KoolBan/Models/Note.cs
@@ -19,6 +19,42 @@ namespace KoolBan.Models
 
         [Display(Name = "Color")]
         public NoteColor NoteColor { get; set; }
+
+        public static string NoteColorToHex(NoteColor color)
+        {
+            switch (color)
+            {
+                case Models.NoteColor.Green:
+                    return "#60a917";
+                case Models.NoteColor.Emerald:
+                    return "#008a00";
+                case Models.NoteColor.Teal:
+                    return "#00aba9";
+                case Models.NoteColor.Cyan:
+                    return "#1ba1e2";
+                case Models.NoteColor.Cobalt:
+                    return "#0050ef";
+                case Models.NoteColor.Indigo:
+                    return "#6a00ff";
+                case Models.NoteColor.Violet:
+                    return "#aa00ff";
+                case Models.NoteColor.Pink:
+                    return "#dc4fad";
+                case Models.NoteColor.Magenta:
+                    return "#d80073";
+                case Models.NoteColor.Red:
+                    return "#e51400";
+                case Models.NoteColor.Orange:
+                    return "#fa6800";
+                case Models.NoteColor.Brown:
+                    return "#825a2c";
+                case Models.NoteColor.Crimson:
+                    return "#a20025";
+                default:
+                    return "#ffffff";
+            }
+    
+        }
     }
 
     public enum NoteColor
@@ -37,5 +73,6 @@ namespace KoolBan.Models
         Orange,
         Brown,
     }
+
 
 }
