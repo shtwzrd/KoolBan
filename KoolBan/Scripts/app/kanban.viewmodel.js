929e744c0184b64cf7e691aa6261f30d414b9216
warreq <brandon.s.lucas@gmail.com>
2014-11-08 01:09:52 +0100

    fixed dragging

--- KoolBan/Scripts/app/kanban.viewmodel.js
+++ KoolBan/Scripts/app/kanban.viewmodel.js
@@ -2,41 +2,44 @@
     var self = this;
     self.poop = ko.observable("Poop");
 
-   /* 
-    Sammy(function () {
-        this.get('#home', function () {
-            // Make a call to the protected Web API by passing in a Bearer Authorization Header
-            $.ajax({
-                method: 'get',
-                url: app.dataModel.userInfoUrl,
-                contentType: "application/json; charset=utf-8",
-                headers: {
-                    'Authorization': 'Bearer ' + app.dataModel.getAccessToken()
-                },
-                success: function (data) {
-                    self.myHometown('Your Hometown is : ' + data.hometown);
-                }
-            });
-        });
-        this.get('/', function () { this.app.runRoute('get', '#home'); });
-    });
-*/
-        $('.tile.main').draggable({
-            revert: 'invalid'
-        });
-        $('td').droppable({
-            accept: '.tile.main',
-            tolerance: 'pointer',
-            drop: function (event, ui) {
-                alert('inside');
-            },
-            over: function (event, ui) {
-                $('#log').text('over');
-            },
-            out: function (event, ui) {
-                $('#log').text('out');
-            }
-        });
+    /* 
+     Sammy(function () {
+         this.get('#home', function () {
+             // Make a call to the protected Web API by passing in a Bearer Authorization Header
+             $.ajax({
+                 method: 'get',
+                 url: app.dataModel.userInfoUrl,
+                 contentType: "application/json; charset=utf-8",
+                 headers: {
+                     'Authorization': 'Bearer ' + app.dataModel.getAccessToken()
+                 },
+                 success: function (data) {
+                     self.myHometown('Your Hometown is : ' + data.hometown);
+                 }
+             });
+         });
+         this.get('/', function () { this.app.runRoute('get', '#home'); });
+     });
+ */
+    $('.tile.main').draggable({
+        revert: 'invalid',
+        snap: '.kanban td',
+        stack: "div",
+});
+
+    $('.kanban td').droppable({
+        accept: '.tile.main',
+        tolerance: 'pointer',
+        drop: function (event, ui) {
+            alert('inside');
+        },
+        over: function (event, ui) {
+            $('#log').text('over');
+        },
+        out: function (event, ui) {
+            $('#log').text('out');
+        }
+    }); 
 
     return self;
 
