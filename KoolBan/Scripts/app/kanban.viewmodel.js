a059b6bf5a88cb2cd363c9f7ec027adc155b20b9
warreq <brandon.s.lucas@gmail.com>
2014-11-12 11:31:24 +0100

    client-side rendering working except colors+logos

--- KoolBan/Scripts/app/kanban.viewmodel.js
+++ KoolBan/Scripts/app/kanban.viewmodel.js
@@ -1,20 +1,60 @@
 ﻿function KanbanViewModel() {
     var self = this;
     self.projectId = $('#projectId')[0].value;
-    self.columns = ko.observableArray([ {ColumnName: "", Notes: [], Capacity: 0 }]);
+    var empty = [
+        { ColumnName: "", Notes: [], Capacity: 0 }
+    ];
+
+    self.columns = ko.observableArray(empty);
 
     app.dataModel.projectId = self.projectId;
 
     self.handleColumnData = function (result) {
         var obs = ko.utils.arrayMap(result, function (item) {
-            return ko.mapping.fromJS(item);
+            return ko.mapping.fromJS(item, mapping);
         });
 
-        self.columns(ko.mapping.toJS(obs));
+        self.columns(ko.mapping.toJS(obs, mapping));
         console.log(self.columns());
     }
 
     app.dataModel.getColumns(self.handleColumnData);
+
+    var Column = function (data) {
+        ko.mapping.fromJS(data, mapping, this);
+    }
+
+    var mapping = {
+        create: function (options) {
+            options.data.AutoSortedNotes = ko.computed(function() {
+                var sorted = options.data.Notes.sort(function(a, b) {
+                     return b.Description.length - a.Description.length;
+                });
+                return sorted;
+            });
+            var column = new Column(options.data);
+
+            return column;
+        },
+        'Notes': {
+            create: function (options) {
+                options.data.NoteClass = ko.computed(function () {
+                    var markup = "tile ";
+                    if (options.data.Description.length > 40) {
+                        markup += "double ";
+                    }
+                    if (options.data.Description.length > 106) {
+                        markup += "double-vertical ";
+                    }
+                    markup += "main ";
+
+                    return markup;
+                });
+                return ko.mapping.fromJS(options.data);
+            }
+        }
+    };
+
 
     $('.tile.main').draggable({
         revert: 'invalid',
